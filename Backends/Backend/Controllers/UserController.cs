da6aa9add2ea63adaef19de38c155dde76551ca9
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2025-04-16 15:24:57 +0200

    created UserController

---
+++ Backends/Backend/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+ using Backend.DTOs;
+ using Backend.interfaces;
+ using Microsoft.AspNetCore.Mvc;
+ using System.Collections.Generic;
+ using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController(IUserService userService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserDto>>> GetAllUsers()
+        {
+            var users = await userService.GetAllUsersAsync();
+            return Ok(users);
+        }
+          [HttpGet("{id}")]
+          public async Task<ActionResult<UserDto>> GetUserById(int id)
+          {
+              var user = await userService.GetUserByIdAsync(id);
+              if (user == null)
+              {
+                  return NotFound();
+              }
+              return Ok(user);
+          }
+          [HttpPost]                    
+          public async Task<ActionResult<UserDto>> CreateUser([FromBody] UserDto userDto)
+          {
+              await userService.AddUserAsync(userDto);
+              return CreatedAtAction(nameof(GetUserById), new { id = userDto.Id }, userDto);
+          }
+          [HttpPut("{id}")]                                                               
+          public async Task<IActionResult> UpdateUser(int id, [FromBody] UserDto userDto)
+          {
+              if (id != userDto.Id)
+              {
+                  return BadRequest();
+              }
+              await userService.UpdateUserAsync(id, userDto);
+              return NoContent();
+          }
+          [HttpDelete("{id}")]
+          public async Task<IActionResult> DeleteUser(int id)
+          {
+              await userService.DeleteUserAsync(id);
+              return NoContent();
+          }
+    }
+}
